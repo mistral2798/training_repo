b7830fc4ac9524a9bd7eb467e1dd365f3d5f0a37
Радислав <mistral2798@mail.ru>
2025-06-29 13:00:44 +0300

    Добавил имя

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import streamlit as st
 # Введение возраста
 age = st.number_input(label='Введите ваш возраст')
 st.write(f'Вам {int(age)} лет')
+
+#Изменение файла (Добавление новых строк)
+name = st.text_input(label='Введите свое имя')
+st.write('Ваше имя:', name)
